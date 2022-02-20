cd6ae914e2ce56e0a8c9907582c346bf17a22243
Chris-hu-liao <66919955+Chris-hu-liao@users.noreply.github.com>
2022-02-20 09:11:31 -0500

    Update and rename Prime_number.java to Caeser Cipher.java

---
+++ Caeser Cipher.java
@@ -0,0 +1,26 @@
+//Every letter in the input has been shifted two places forward as specified in the input.
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc =new Scanner(System.in);
+	    System.out.print("Enter a sentence: ");
+	    String plaintext=sc.nextLine();
+	    int shift=2;
+	    String ciphertext="";
+	    char alphabet;
+	    for(int i=0; i<plaintext.length(); i++)
+	    {
+	        alphabet=plaintext.charAt(i);
+	        if (alphabet>='a' && alphabet<='z'){
+	            alphabet=(char)(alphabet+shift);
+	            if (alphabet>'z'){
+	                alphabet=(char)(alphabet+'a'-'z'-1);
+	                
+	            }
+	            System.out.print(alphabet);
+	        }
+	    }
+		
+	}
+}
